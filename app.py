96c7fac2a7a056d8c6a47ff15b330a8b1e405184
Spandita Saha <spandita406@gmail.com>
2023-05-30 20:58:17 +0530

    changed

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_mod
 
 #instatiate flask app  
 app = Flask(__name__, template_folder='./templates')
-env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object("flask_project.config.DevelopmentConfig")
+env_config = os.getenv("PROD_APP_SETTINGS")
 app.config.from_object(env_config)
 
 
